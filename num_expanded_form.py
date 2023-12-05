fe8fdca8e904484ac41b48b5f6cd6670b75fddd9
saradha <saradhashyam0401@gmail.com>
2023-12-05 17:27:48 +0530

    partitioning the number and expanding

---
+++ num_expanded_form.py
@@ -0,0 +1,15 @@
+def expanded_form(num):
+        lst = [] # to store the values
+        l = len(str(num)) # getting the lenght of num by converting to string
+
+        for indx, dig in enumerate(str(num)):
+            dig = int(dig) # converting to digit to perform the calculation
+            if dig != 0: # doing operation if the digit is not 0
+                print(indx)
+                lst.append(dig*(10**((l-indx)-1))) # appending to list the 0's to be added 
+                print(lst)
+        result_string = ' + '.join(map(str, lst))  # converting to string and joining them by +
+        print(result_string)
+        return result_string
+
+expanded_form(70304)
