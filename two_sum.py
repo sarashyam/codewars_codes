6cc4878f6bb33ddd95c2be01bb9492cbd6b3d729
saradha <saradhashyam0401@gmail.com>
2023-12-05 16:01:44 +0530

    code optimized

--- two_sum.py
+++ two_sum.py
@@ -8,24 +8,33 @@ The input will always be valid (numbers will be an array of length 2 or greater,
 
 def two_sum(numbers, target):
     
-    a=1
-    for num in numbers:
-            print(len(numbers))
-            diff_val = target - num 
-            ind = numbers.index(num)
-            print(f"num {num}, diff value {diff_val}")
-            if diff_val in numbers:
-                if numbers.index(diff_val) == ind: 
-                    a = ind, reversed_index = len(numbers) - 1 - numbers[::-1].index(diff_val), numbers.index(diff_val)
-                else:
-                    a = ind, numbers.index(diff_val)
-                print(a)
-                if a[0] == a[1]:
-                    continue
-                break
+    # a=1
+    # for num in numbers:
+    #         print(len(numbers))
+    #         diff_val = target - num 
+    #         ind = numbers.index(num)
+    #         print(f"num {num}, diff value {diff_val}")
+    #         if diff_val in numbers:
+    #             if numbers.index(diff_val) == ind: 
+    #                 a = ind, reversed_index = len(numbers) - 1 - numbers[::-1].index(diff_val), numbers.index(diff_val)
+    #             else:
+    #                 a = ind, numbers.index(diff_val)
+    #             print(a)
+    #             if a[0] == a[1]:
+    #                 continue
+    #             break
             
             
-    return a
+    # return a
+
+    for i, num in enumerate(numbers):
+        diff_val = target - num
+        if diff_val in numbers[i + 1:]:
+            return i, numbers.index(diff_val, i + 1)
+    return None
+
+
+
 
 z = two_sum([1234,5678,9012], 14690)
 print(z)
